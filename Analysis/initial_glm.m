c735d4da4766db954b0f9f58a197429b2ff1c7c7
Annie <annie.marazita.2024@owu.edu>
2025-02-04 18:29:37 -0500

    Initial GLM updates and summary plotting
    Number of predictors, window size, weak cue locations, statistical significance

--- Analysis/initial_glm.m
+++ Analysis/initial_glm.m
@@ -6,10 +6,11 @@ for unit_idx = 1:length(unit_data)
     %% 1. Set-up.
     % Choose the unit
     data = unit_data(unit_idx);
-    valid_trials = ~isnan(data.ids.sample_id); % AODR only
+    cue_loc = data.ids.sample_id;
+    valid_trials = ismember(cue_loc,[-1, 0, 1]); % AODR with weak evidence only
         
     % Choose 300ms sliding window times
-    window_size = 300;
+    window_size = 150;
     step_size = 10;
     sac_on_idx = data.times.sac_on(valid_trials)*1000;
     earliest_sac_on = round(min(sac_on_idx));
@@ -20,42 +21,53 @@ for unit_idx = 1:length(unit_data)
     coeffs = cell(num_windows, 1); % Will store coefficients from each window's model
     
     %% 2. Create a table for the unit to run the model.
-    % a) Cue location: -4, -3, -2, -1, 0, 1, 2, 3, or 4
-    cue_loc = data.ids.sample_id;
+    % a) Cue location: -1, 0, or 1
     cue_loc = cue_loc(valid_trials);
     
     % b) Hazard rate: 0.5 or -0.5
     hazard = data.values.hazard(valid_trials);
     hazard(hazard==0.05) = -0.5; % Center the variable
-    
-    % c) Firing rate: baseline-subtract, z-scored firing rate over a given
+
+    % c) Choice: -0.5 (stay) or 0.5 (switch)
+    % Information needed to determine whether a choice was switch
+    prevH = [nan; data.values.hazard(1:end-1)];
+    thisH = data.values.hazard;
+    prevState = [nan; data.ids.correct_target(1:end-1)];
+    choice = data.ids.choice;
+    is_switch = (thisH==prevH) & (prevState ~= choice); % same block and chose target opposite previously correct one
+    choice(is_switch) = 0.5; % recode
+    choice(~is_switch) = -0.5;
+    choice = choice(valid_trials);
+
+    % d) Firing rate: baseline-subtract, z-scored firing rate over a given
     % evoked window after cue onset
     % i) Subtract the baseline firing rate
     evoked = squeeze(data.binned_spikes); % ms x all trials
     evoked = evoked(:, valid_trials); % ms x valid trials
     baseline = data.epochs.baseline(valid_trials);
     baseline_subtracted = evoked - baseline;
+
+    % ii) Z-score (keep interpretation consistent)
+    mu = mean(baseline_subtracted, "all", "omitnan"); % scalar, using the full trial duration
+    sigma = nanstd(baseline_subtracted, 0, 'all'); % scalar
     
     for window_num = 1:num_windows
     
-        % ii) Isolate evoked window of interest
+        % iii) Isolate evoked window of interest
         window_start = window_starts(window_num);
         window_end = window_ends(window_num);
     
-        baseline_subtracted_window = baseline_subtracted(window_start:window_end, :);
-        mean_baseline_subtracted = mean(baseline_subtracted_window, 1, "omitnan"); % 1 x trials
-    
-        % iii) Z-score (keep interpretation consistent)
-        mu = mean(mean_baseline_subtracted, "omitnan"); % average over all trials
-        sigma = std(mean_baseline_subtracted, "omitnan"); % std over all trials
-        z_scored = (mean_baseline_subtracted - mu) ./ sigma; % 1 x trials
+        baseline_subtracted_window = baseline_subtracted(window_start:window_end, :); % window_size x valid_trials
+        mean_baseline_subtracted = mean(baseline_subtracted_window, 1, "omitnan"); % 1 x valid_trials
+        
+        z_scored = (mean_baseline_subtracted - mu) ./ sigma; % 1 x valid_trials
         fr = z_scored';
         
         % iv) Combine predictor and response variables into design matrix
-        X = table(cue_loc, hazard, fr, 'VariableNames', {'cue_loc', 'hazard', 'fr'});
+        X = table(cue_loc, hazard, choice, fr, 'VariableNames', {'cue_loc', 'hazard', 'choice', 'fr'});
         
         %% 3. Run the model, saving results.
-        mdl = fitlm(X, 'fr ~ cue_loc*hazard');
+        mdl = fitlm(X, 'fr ~ cue_loc + hazard + choice + cue_loc*hazard + cue_loc*hazard*choice');
         coeffs{window_num} = mdl.Coefficients;
     
     end
--- Plotting/plot_initial_glm.m
+++ Plotting/plot_initial_glm.m
@@ -15,24 +15,54 @@ sessionName = filename(startIdx:endIdx);
 % Initialize arrays to hold the coefficients for plotting
 cue_loc_coeffs = nan(num_windows, 1);
 hazard_coeffs = nan(num_windows, 1);
+choice_coeffs = nan(num_windows, 1);
 cue_hazard_coeffs = nan(num_windows, 1);
+cue_h_choice_coeffs = nan(num_windows, 1);
+
 cue_loc_SEM = nan(num_windows, 1);
 hazard_SEM = nan(num_windows, 1);
+choice_SEM = nan(num_windows, 1);
 cue_hazard_SEM = nan(num_windows, 1);
+cue_h_choice_SEM = nan(num_windows, 1);
 
-% Get coefficients for each model/window
+cue_loc_ps = nan(num_windows, 1);
+hazard_ps = nan(num_windows, 1);
+choice_ps = nan(num_windows, 1);
+cue_hazard_ps = nan(num_windows, 1);
+cue_h_choice_ps = nan(num_windows, 1);
+
+% Get coefficients, SE, and p-value for each model/window
 for i = 1:num_windows
 
     coeff_table = results{i};
+
     cue_loc_coeffs(i) = coeff_table.Estimate(2);
     hazard_coeffs(i) = coeff_table.Estimate(3);
-    cue_hazard_coeffs(i) = coeff_table.Estimate(4);
+    choice_coeffs(i) = coeff_table.Estimate(4);
+    cue_hazard_coeffs(i) = coeff_table.Estimate(5);
+    cue_h_choice_coeffs(i) = coeff_table.Estimate(8);
 
     cue_loc_SEM(i) = coeff_table.SE(2);
     hazard_SEM(i) = coeff_table.SE(3);
-    cue_hazard_SEM(i) = coeff_table.SE(4);
+    choice_SEM(i) = coeff_table.SE(4);
+    cue_hazard_SEM(i) = coeff_table.SE(5);
+    cue_h_choice_SEM(i) = coeff_table.SE(8);
+
+    cue_loc_ps(i) = coeff_table.pValue(2);
+    hazard_ps(i) = coeff_table.pValue(3);
+    choice_ps(i) = coeff_table.Estimate(4);
+    cue_hazard_ps(i) = coeff_table.pValue(5);
+    cue_h_choice_ps(i) = coeff_table.pValue(8);
 
 end
+
+% Determine which are significant
+alpha = 0.05;
+is_significant_cue_loc = cue_loc_ps < alpha;
+is_significant_hazard = hazard_ps < alpha;
+is_significant_choice = choice_ps < alpha;
+is_significant_cue_hazard = cue_hazard_ps < alpha;
+is_significant_cue_h_choice = cue_h_choice_ps < alpha;
 
 % Plot
 fig = figure;
@@ -42,7 +72,9 @@ hold on;
 xaxis = (0:(num_windows - 1)) * step_size;
 plot(xaxis, cue_loc_coeffs, 'b', 'DisplayName', 'cue loc', 'LineWidth', 2);
 plot(xaxis, hazard_coeffs, 'r', 'DisplayName', 'hazard', 'LineWidth', 2);
+plot(xaxis, choice_coeffs, 'y', 'DisplayName', 'choice', 'LineWidth', 2);
 plot(xaxis, cue_hazard_coeffs, 'm', 'DisplayName', 'cue loc:hazard', 'LineWidth', 2);
+plot(xaxis, cue_h_choice_coeffs, 'k', 'DisplayName', 'cue loc:hazard:choice', 'LineWidth', 2);
 
 % Error bands
 fill([xaxis, flip(xaxis)], ...
@@ -52,8 +84,35 @@ fill([xaxis, flip(xaxis)], ...
      [hazard_coeffs' + hazard_SEM', flip(hazard_coeffs' - hazard_SEM')], ...
      'r', 'FaceAlpha', 0.2, 'EdgeColor', 'none',  'HandleVisibility', 'off');
 fill([xaxis, flip(xaxis)], ...
+     [choice_coeffs' + hazard_SEM', flip(choice_coeffs' - choice_SEM')], ...
+     'y', 'FaceAlpha', 0.2, 'EdgeColor', 'none',  'HandleVisibility', 'off');
+fill([xaxis, flip(xaxis)], ...
      [cue_hazard_coeffs' + cue_hazard_SEM', flip(cue_hazard_coeffs' - cue_hazard_SEM')], ...
      'm', 'FaceAlpha', 0.2, 'EdgeColor', 'none',  'HandleVisibility', 'off');
+fill([xaxis, flip(xaxis)], ...
+     [cue_h_choice_coeffs' + cue_h_choice_SEM', flip(cue_h_choice_coeffs' - cue_h_choice_SEM')], ...
+     'k', 'FaceAlpha', 0.2, 'EdgeColor', 'none',  'HandleVisibility', 'off');
+
+% Plot small asterisks for significant coefficients
+sig_height = max(max([cue_loc_coeffs, hazard_coeffs, choice_coeffs, cue_hazard_coeffs, cue_h_choice_coeffs])) + 0.05;
+sig_heights = [sig_height * 0.90, sig_height * 0.95, sig_height, sig_height * 1.05, sig_height * 1.1];
+for i = 1:num_windows
+    if is_significant_cue_loc(i)
+        text(xaxis(i), sig_heights(1), '*', 'Color', 'b', 'FontSize', 12, 'HorizontalAlignment', 'center');
+    end
+    if is_significant_hazard(i)
+        text(xaxis(i), sig_heights(2), '*', 'Color', 'r', 'FontSize', 12, 'HorizontalAlignment', 'center');
+    end
+    if is_significant_choice(i)
+        text(xaxis(i), sig_heights(3), '*', 'Color', 'y', 'FontSize', 12, 'HorizontalAlignment', 'center');
+    end
+    if is_significant_cue_hazard(i)
+        text(xaxis(i), sig_heights(4), '*', 'Color', 'm', 'FontSize', 12, 'HorizontalAlignment', 'center');
+    end
+    if is_significant_cue_h_choice(i)
+        text(xaxis(i), sig_heights(5), '*', 'Color', 'k', 'FontSize', 12, 'HorizontalAlignment', 'center');
+    end
+end
 
 % Customize the plot
 xlabel('Time from Cue Onset (ms)');
