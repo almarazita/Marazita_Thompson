a13710e884a2cb048a3f841805510aeecbca3f30
Annie <annie.marazita.2024@owu.edu>
2025-03-18 22:12:20 -0400

    Added extreme cue locations to theory predictions

--- Analysis/theory_prediction.m
+++ Analysis/theory_prediction.m
@@ -12,7 +12,7 @@ neFromBelief = @(belief) negentropy(pFromBelief(belief));
 
 %% Calculate
 % Setup
-h = 0.5;
+h = 0.50;
 prior = log(h ./ (1 - h));
 cues = [-inf, -0.4771, 0, 0.4771, inf];
 cue_names = ["Strong T1", "Weak T1", "Ambiguous", "Weak T2", "Strong T2"];
@@ -38,6 +38,23 @@ for cue_loc = 1:5
         ic(cue_loc), ir(cue_loc));
 end
 
+% Redo for extreme cue locations that threw NaNs
+negentropy_ref = negentropy(h);
+ic(1) = 0 - negentropy_ref;
+ic(5) = 0 - negentropy_ref;
+xaxis = 0:0.001:1;
+yaxis = negentropy(xaxis);
+dy = diff(yaxis); dx = diff(xaxis); dy_dx = dy ./ dx;
+grad_ref = dy_dx(xaxis == h);
+
+dx = pFromBelief(cur_beliefs(1)) - h;
+ir(1) = -1*grad_ref*dx;
+kl(1) = ic(1) + ir(1);
+
+dx = pFromBelief(cur_beliefs(5)) - h;
+ir(5) = -1*grad_ref*dx;
+kl(5) = ic(5) + ir(5);
+
 %% Plot
 % Bar graphs with quantities grouped together in rows
 %colors = ["#20419A", "#437DB7", "w", "#79ABC5", "#70CDDD"]; % low-switch
@@ -47,75 +64,71 @@ edge_colors = ["none", "none", "k", "none", "none"];
 figure;
 subplot(311);
 bar(ic);
-ylim([1.1*-0.2406, 1.1*0.3616]); % based on overall max/min values
+ylim([1.1*-0.2406, 1.1*2.9858]); % based on overall max/min values
 subplot(312);
 bar(ir);
-ylim([1.1*-0.2406, 1.1*0.3616]);
+ylim([1.1*-0.2406, 1.1*2.9858]);
 subplot(313);
 bar(kl);
-ylim([1.1*-0.2406, 1.1*0.3616]);
+ylim([1.1*-0.2406, 1.1*2.9858]);
 
 % Bar graph for each cue location
-% for cue_loc = 1:5
-%     figure;
-%     bar([ic(cue_loc), ir(cue_loc), kl(cue_loc)], 'FaceColor', ...
-%         colors(cue_loc), 'EdgeColor', edge_colors(cue_loc), ...
-%         'DisplayName', cue_names(cue_loc));
-%     ylim([1.1*-0.2406, 1.1*0.3616]);
-%     xticklabels(["Certainty", "Disparity", "DKL"]);
-%     legend();
-% end
+for cue_loc = 1:5
+    figure;
+    bar([ic(cue_loc), ir(cue_loc), kl(cue_loc)], 'FaceColor', ...
+        colors(cue_loc), 'EdgeColor', edge_colors(cue_loc), ...
+        'DisplayName', cue_names(cue_loc));
+    ylim([1.1*-0.2406, 1.1*0.3616]);
+    xticklabels(["Certainty", "Disparity", "DKL"]);
+    legend();
+end
 
 % 1D belief updates
-% figure;
-% hold on;
-% 
-% colors = ["#20419A", "#437DB7", "w", "#79ABC5", "#70CDDD"]; % low-switch
-% %colors = ["#8C191B", "#CB2027", "w", "#E32026", "#EF8480"]; %
-% high-switch
-% 
-% p1 = plot([0, 1], [0, 0], 'k-', 'LineWidth', 2);
-% p2 = plot([0, 0], [-0.1, 0.1], 'k-', 'LineWidth', 2);
-% p3 = plot([0.5, 0.5], [-0.1, 0.1], 'k-', 'LineWidth', 2);
-% p4 = plot([1, 1], [-0.1, 0.1], 'k-', 'LineWidth', 2);
-% scatter(h, 0, 100, 'filled', 'MarkerFaceColor', 'k', ...
-%     'DisplayName', 'Prior = 0.13');
-% for cue_loc = 1:5
-%     if cue_loc == 3
-%         scatter(pFromBelief(cur_beliefs(cue_loc)), 0, 100, ...
-%             'filled', 'MarkerFaceColor', colors(cue_loc), ...
-%             'MarkerEdgeColor', 'k', 'DisplayName', cue_names(cue_loc));
-%     else
-%         scatter(pFromBelief(cur_beliefs(cue_loc)), 0, 100, ...
-%         'filled', 'MarkerFaceColor', colors(cue_loc), ...
-%         'DisplayName', cue_names(cue_loc));
-%     end
-% end
-% 
-% ylim([-1 1]);
-% set(gca, 'XColor', 'k', 'YColor', 'k');
-% text(h, -0.1, 'P', 'Color', 'k', 'FontSize', 14, 'FontWeight', 'bold', 'HorizontalAlignment', 'center');
-% p1.HandleVisibility = 'off';
-% p2.HandleVisibility = 'off';
-% p3.HandleVisibility = 'off';
-% p4.HandleVisibility = 'off';
-% legend();
+figure;
+hold on;
+
+p1 = plot([0, 1], [0, 0], 'k-', 'LineWidth', 2);
+p2 = plot([0, 0], [-0.1, 0.1], 'k-', 'LineWidth', 2);
+p3 = plot([0.5, 0.5], [-0.1, 0.1], 'k-', 'LineWidth', 2);
+p4 = plot([1, 1], [-0.1, 0.1], 'k-', 'LineWidth', 2);
+scatter(h, 0, 100, 'filled', 'MarkerFaceColor', 'k', ...
+    'DisplayName', 'Prior = 0.13');
+for cue_loc = 1:5
+    if cue_loc == 3
+        scatter(pFromBelief(cur_beliefs(cue_loc)), 0, 100, ...
+            'filled', 'MarkerFaceColor', colors(cue_loc), ...
+            'MarkerEdgeColor', 'k', 'DisplayName', cue_names(cue_loc));
+    else
+        scatter(pFromBelief(cur_beliefs(cue_loc)), 0, 100, ...
+        'filled', 'MarkerFaceColor', colors(cue_loc), ...
+        'DisplayName', cue_names(cue_loc));
+    end
+end
+
+ylim([-1 1]);
+set(gca, 'XColor', 'k', 'YColor', 'k');
+text(h, -0.1, 'P', 'Color', 'k', 'FontSize', 14, 'FontWeight', 'bold', 'HorizontalAlignment', 'center');
+p1.HandleVisibility = 'off';
+p2.HandleVisibility = 'off';
+p3.HandleVisibility = 'off';
+p4.HandleVisibility = 'off';
+legend();
 
 % 2D negentropy curve
-% figure;
-% hold on;
-% xaxis = 0:0.001:1;
-% yaxis = negentropy(xaxis);
-% 
-% p = plot(xaxis, yaxis, 'DisplayName', '');
-% scatter(h, negentropy(h), 'DisplayName', 'Prior = 0.33');
-% for cue_loc = 1:5
-%     scatter(pFromBelief(cur_beliefs(cue_loc)), ...
-%         neFromBelief(cur_beliefs(cue_loc)), ...
-%         'DisplayName', cue_names(cue_loc));
-% end
-% 
-% p.HandleVisibility = 'off';
-% legend();
-% xlabel('P(Target 2)');
-% ylabel('Negative entropy ("certainty")');
+figure;
+hold on;
+
+p = plot(xaxis, yaxis, 'DisplayName', '');
+scatter(h, negentropy(h), 'DisplayName', 'Prior = 0.50');
+certaintys = neFromBelief(cur_beliefs);
+certaintys(1) = 0; certaintys(5) = 0;
+for cue_loc = 1:5
+    scatter(pFromBelief(cur_beliefs(cue_loc)), ...
+        certaintys(cue_loc), ...
+        'DisplayName', cue_names(cue_loc));
+end
+
+p.HandleVisibility = 'off';
+legend();
+xlabel('P(Target 2)');
+ylabel('Negative entropy ("certainty")');
